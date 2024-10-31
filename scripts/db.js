019af063f46a2d55e83458190da9f2db8fe4ab84
Dennis Jakobi <dennis.jakobi76@outlook.de>
2024-10-31 16:06:36 +0100

    added aot-section and more icons

---
+++ scripts/db.js
@@ -0,0 +1,38 @@
+let myDishes = [
+    {
+        "name": "Spaghetti Carbonara",
+        "price": 12.99,
+        "description": "Leckere Pasta mit Speck und Parmesan.",
+        "category": "mainDish",
+    },
+    {
+        "name": "Vegetarische Lasagne",
+        "price": 10.5,
+        "description": "Herzhafte Lasagne mit Schichten aus Gemüse.",
+        "category": "mainDish",
+    },
+    {
+        "name": "Pommes Frites",
+        "price": 5.8,
+        "description": "Der Klassiker für alle, die sich nicht entscheiden können.",
+        "category": "mainDish",
+    },
+    {
+        "name": "Tiramisu",
+        "price": 7.9,
+        "description": "Ein Traum aus abwechselnden Schichten von Lööfelbisquit und einer köstlichen Creme.",
+        "category": "dessert",
+    },
+    {
+        "name": "Panna Cotta",
+        "price": 6.22,
+        "description": "Ein Seelenschmeichler aus aufgekochter Sahne, der die Sinne verführt.",
+        "category": "dessert",
+    },
+    {
+        "name": "Schoko-Pudding",
+        "price": 5.19,
+        "description": "Der Nachtisch wie von Omi. In Erinnerungen schwelgen und einfach genießen.",
+        "category": "dessert",
+    },
+];
